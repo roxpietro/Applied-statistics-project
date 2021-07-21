809492dc84bd43a31da6b5c9532d642c02f544a8
Teresa Babini <teresa.babini@mail.polimi.it>
2021-07-21 10:21:47 +0000

    home outliers

--- ANALISI FUNZIONALE STOPS/Analisi outliers.R
+++ ANALISI FUNZIONALE STOPS/Analisi outliers.R
@@ -47,9 +47,10 @@ days[7,]<-out1$popularity_by_hour_sunday[[1]]
 days<-t(days)
 my_col <- rainbow(7)
 x11()
-matplot(days,type='l', col = my_col) #7 giorni settimanali, già fatta la media dentro popularity ...
+png(file = "popularity matplot.png")
+matplot(days,type='l', col = my_col, main = " Dyas popularity in CBG 360610031001") #7 giorni settimanali, già fatta la media dentro popularity ...
 legend("topleft", legend= c('mon', 'tue','wed', 'thu', 'fri', 'sat', 'sun'), fill = my_col)
-
+dev.off()
 lun <- matrix(nrow = 5, ncol=24)
 for (i in 1:5){
   inizio <- seq(1,720,24*7)[i]
@@ -64,7 +65,7 @@ for (i in 1:4){
   mar[i,] <- out1$stops_by_each_hour[[1]][inizio:fine]
 }
 mer <- matrix(nrow = 4, ncol=24)
-for (i in 1:5){
+for (i in 1:4){
   inizio <- seq(49,720,24*7)[i]
   fine <- seq(72,720,24*7)[i]
   mer[i,] <- out1$stops_by_each_hour[[1]][inizio:fine]
@@ -76,7 +77,7 @@ for (i in 1:4){
   gio[i,] <- out1$stops_by_each_hour[[1]][inizio:fine]
 }
 ven <- matrix(nrow = 4, ncol=24)
-for (i in 1:5){
+for (i in 1:4){
   inizio <- seq(97,720,24*7)[i]
   fine <- seq(120,720,24*7)[i]
   ven[i,] <- out1$stops_by_each_hour[[1]][inizio:fine]
@@ -104,30 +105,203 @@ dom <- t(dom)
 
 library(RColorBrewer)
 col = brewer.pal(n = 8, name = 'YlOrRd')
+setwd("/home/terri/Documenti/UNIVERSITA/STAT APP/progetto/gitcode/Applied-statistics-project/ANALISI FUNZIONALE STOPS/Plot/outliers 92")
 x11()
-matplot(lun,type='l', col = col[3:8], lwd =2)
+png(file = 'Monday.png')
+matplot(lun,type='l', col = col[3:8], lwd =2, main = 'Monday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'YlGnBu')
+x11()
+png(file = 'Tuesday.png')
+matplot(mar,type='l', col = col[4:8],  lwd =2, main = 'Tuesday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'YlGn')
+x11()
+png(file = 'Wednesday.png')
+matplot(mer,type='l', col = col[4:8], lwd=2, main = 'Wednesday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'RdPu')
+x11()
+png(file = 'Thursday.png')
+matplot(gio,type='l', col = col[4:8], lwd=2, main = 'Thursday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'OrRd')
+x11()
+png(file = 'Friday.png')
+matplot(ven,type='l', col = col[4:8], lwd=2, main = 'Friday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'Greens')
+x11()
+png(file = 'Saturday.png')
+matplot(sab,type='l', col = col[4:8], lwd=2, main = 'Saturday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'Blues')
+x11()
+png(file = 'Sunday.png')
+matplot(dom,type='l', col = col[4:8], lwd=2, main = 'Sunday')
+dev.off()
 
-col = brewer.pal(n = 8, name = 'YlGnBu')
-x11()
-matplot(mar,type='l', col = col[4:8],  lwd =2)
+#non so che grandi commenti si possano fare. mi viene in mente:
+# - le prime settimane (quelle chiare) hanno tante persone anche se c'erano più 
+#   restrizioni sugli spostamenti -> proteste articoli forse?
+# - nel fine settimana in generale si vedono più persone
+# - grande afflusso di gente (anche ora tarda), principalmente la sera -> forse 
+#   perchè è zona di passaggio per ritorno a casa (vd ponte strade, Brooklyn Bridge)
 
-col = brewer.pal(n = 8, name = 'YlGn')
+days<-matrix(nrow = 7, ncol=24)
+days[1,]<-out2$popularity_by_hour_monday[[1]]
+days[2,]<-out2$popularity_by_hour_tuesday[[1]]
+days[3,]<-out2$popularity_by_hour_wednesday[[1]]
+days[4,]<-out2$popularity_by_hour_thursday[[1]]
+days[5,]<-out2$popularity_by_hour_friday[[1]]
+days[6,]<-out2$popularity_by_hour_saturday[[1]]
+days[7,]<-out2$popularity_by_hour_sunday[[1]]
+days<-t(days)
+my_col <- rainbow(7)
 x11()
-matplot(mer,type='l', col = col[4:8], lwd=2)
+png(file = "popularity matplot.png")
+matplot(days,type='l', col = my_col, main = "Days popularity in CBG 360610143001" ) #7 giorni settimanali, già fatta la media dentro popularity ...
+legend("topleft", legend= c('mon', 'tue','wed', 'thu', 'fri', 'sat', 'sun'), fill = my_col)
+dev.off()
+lun <- matrix(nrow = 5, ncol=24)
+for (i in 1:5){
+  inizio <- seq(1,720,24*7)[i]
+  fine <- seq(24,720,24*7)[i]
+  lun[i,] <- out2$stops_by_each_hour[[1]][inizio:fine]
+}
+lun[4,]
+mar <- matrix(nrow = 4, ncol=24)
+for (i in 1:4){
+  inizio <- seq(25,720,24*7)[i]
+  fine <- seq(48,720,24*7)[i]
+  mar[i,] <- out2$stops_by_each_hour[[1]][inizio:fine]
+}
+mer <- matrix(nrow = 4, ncol=24)
+for (i in 1:4){
+  inizio <- seq(49,720,24*7)[i]
+  fine <- seq(72,720,24*7)[i]
+  mer[i,] <- out2$stops_by_each_hour[[1]][inizio:fine]
+}
+gio <- matrix(nrow = 4, ncol=24)
+for (i in 1:4){
+  inizio <- seq(73,720,24*7)[i]
+  fine <- seq(96,720,24*7)[i]
+  gio[i,] <- out2$stops_by_each_hour[[1]][inizio:fine]
+}
+ven <- matrix(nrow = 4, ncol=24)
+for (i in 1:4){
+  inizio <- seq(97,720,24*7)[i]
+  fine <- seq(120,720,24*7)[i]
+  ven[i,] <- out2$stops_by_each_hour[[1]][inizio:fine]
+}
+sab <- matrix(nrow = 4, ncol=24)
+for (i in 1:4){
+  inizio <- seq(121,720,24*7)[i]
+  fine <- seq(144,720,24*7)[i]
+  sab[i,] <- out2$stops_by_each_hour[[1]][inizio:fine]
+}
+dom <- matrix(nrow = 4, ncol=24)
+for (i in 1:4){
+  inizio <- seq(145,720,24*7)[i]
+  fine <- seq(168,720,24*7)[i]
+  dom[i,] <- out2$stops_by_each_hour[[1]][inizio:fine]
+}
 
-col = brewer.pal(n = 8, name = 'RdPu')
-x11()
-matplot(gio,type='l', col = col[4:8], lwd=2)
+lun <- t(lun)
+mar <- t(mar)
+mer <- t(mer)
+gio <- t(gio)
+ven <- t(ven)
+sab <- t(sab)
+dom <- t(dom)
 
-col = brewer.pal(n = 8, name = 'OrRd')
+library(RColorBrewer)
+col = brewer.pal(n = 8, name = 'YlOrRd')
+setwd("/home/terri/Documenti/UNIVERSITA/STAT APP/progetto/gitcode/Applied-statistics-project/ANALISI FUNZIONALE STOPS/Plot/outlier 532")
 x11()
-matplot(ven,type='l', col = col[4:8], lwd=2)
+png(file = 'Monday.png')
+matplot(lun,type='l', col = col[3:8], lwd =2, main = 'Monday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'YlGnBu')
+x11()
+png(file = 'Tuesday.png')
+matplot(mar,type='l', col = col[4:8],  lwd =2, main = 'Tuesday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'YlGn')
+x11()
+png(file = 'Wednesday.png')
+matplot(mer,type='l', col = col[4:8], lwd=2, main = 'Wednesday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'RdPu')
+x11()
+png(file = 'Thursday.png')
+matplot(gio,type='l', col = col[4:8], lwd=2, main = 'Thursday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'OrRd')
+x11()
+png(file = 'Friday.png')
+matplot(ven,type='l', col = col[4:8], lwd=2, main = 'Friday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'Greens')
+x11()
+png(file = 'Saturday.png')
+matplot(sab,type='l', col = col[4:8], lwd=2, main = 'Saturday')
+dev.off()
+#col = brewer.pal(n = 8, name = 'Blues')
+x11()
+png(file = 'Sunday.png')
+matplot(dom,type='l', col = col[4:8], lwd=2, main = 'Sunday')
+dev.off()
 
-col = brewer.pal(n = 8, name = 'Greens')
-x11()
-matplot(sab,type='l', col = col[4:8], lwd=2)
+#anche qua che commneti boh, andamento abbastanza più regolare con picco sempre alla sera nel pre cena, alti flussi sempre anche mattina molto presto
 
-col = brewer.pal(n = 8, name = 'Blues')
+### guardiamo da dove arriva la gente che va lì: HOME
+load("/home/terri/Documenti/UNIVERSITA/STAT APP/progetto/gitcode/Applied-statistics-project/DATASET/Conversione dal dataset originale ad adesso/Cyber_Capital.RData")
+load("/home/terri/Documenti/UNIVERSITA/STAT APP/progetto/gitcode/Applied-statistics-project/DATASET/Complete_dataset.RData")
+rm(patterns_ny)
+rm(census_metadata)
+# order patterns_ny and census_block_ny by CBG of New York County
+complete_dataset = complete_dataset[order(complete_dataset$area),]
+census_blocks_ny = census_blocks_ny[order(census_blocks_ny$CensusBlockGroup),]
+#remove not common cbg
+remove <- which(census_blocks_ny$CensusBlockGroup %in% complete_dataset$area == FALSE)
+census_blocks_ny <- census_blocks_ny[-remove,]
+dim(census_blocks_ny)
+
+library(geosphere)
+library(sf)
+library(sp)           ## Data management
+library(lattice)      ## Data management
+library(geoR)         ## Geostatistics
+library(gstat)        ## Geostatistics
+library(ggplot2)
+library(raster)
+library(rgdal)
+#coordinate in utm
+centroids <- st_centroid(census_blocks_ny$geometry, of_largest_polygon = FALSE)
+coord <- as.numeric(unlist(centroids))
+coord.x_long <- coord[seq(1,length(coord),by=2)]
+coord.y_lat <- coord[seq(2,length(coord),by=2)]
+coord<-SpatialPoints(cbind(coord.x_long,coord.y_lat),proj4string=CRS("+proj=longlat"))
+coord.UTM <- spTransform(coord, CRS("+proj=utm +zone=18 +datum=WGS84"))
+
+coord.x <- coord.UTM@coords[,1]
+coord.y <- coord.UTM@coords[,2]
+
+#out 1: 92 quello  grande vicino a Brookyln Bridge
+home <- names(out1$device_home_areas[[1]])
+index_home <- c()
+tot <- 0 #già si vede che su 4892 cbg home diverse ne trova nello stato di NY 3125
+for (i in 1: length(home)){
+  k <- which(census_blocks_ny$CensusBlockGroup ==home[i])
+  if (length(k) != 0){
+    tot <- tot+1
+    index_home[tot] <-k
+  }
+}
+
 x11()
-matplot(dom,type='l', col = col[4:8], lwd=2)
-detach(New_York_County_no_river)
+ggplot() + 
+  geom_sf(data = census_blocks_ny$geometry) +
+  geom_sf(data = census_blocks_ny$geometry[index_home], fill="red")+
+  geom_sf(data = census_blocks_ny$geometry[which(census_blocks_ny$CensusBlockGroup == cbg_out[1]),], fill="yellow")
