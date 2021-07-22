afa6e3749492f04c0b158085a97a0bb8db346d3a
Teresa Babini <teresa.babini@mail.polimi.it>
2021-07-22 08:09:20 +0000

    cont outliers

---
+++ REGRESSIONE/analysis_home.R
@@ -0,0 +1,7 @@
+setwd("~/Documenti/UNIVERSITA/STAT APP/progetto/gitcode/Applied-statistics-project/REGRESSIONE")
+
+load("/home/terri/Documenti/UNIVERSITA/STAT APP/progetto/gitcode/Applied-statistics-project/DATASET/NYC_no_river.RData")
+load("/home/terri/Documenti/UNIVERSITA/STAT APP/progetto/gitcode/Applied-statistics-project/DATASET/CBG_NY_no_river.RData")
+load("/home/terri/Documenti/UNIVERSITA/STAT APP/progetto/gitcode/Applied-statistics-project/DATASET/River_Dataset.RData")
+
+#vediamo distance form home vs quante home non nello stato di New York (di Manhattan)
